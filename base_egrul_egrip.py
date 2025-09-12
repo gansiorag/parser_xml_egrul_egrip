d3c7d21183ccffd64fe517ca915f5b815e4c2292
gansior <gansior@exportcenter.ru>
2025-09-12 18:58:25 +0300

    add ch

---
+++ base_egrul_egrip.py
@@ -0,0 +1,73 @@
+import os, sys
+from datetime import datetime
+import json
+import logging
+
+
+new_path = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(new_path + "/")
+print(new_path)
+new_path2 = new_path + "/modules/"
+sys.path.append(new_path2)
+
+from modules.egrul.egrul_moduls import parser_mesage_egrul
+from modules.egrul.egrip_moduls import parser_mesage_egrip
+from modules.egrul.com_f import get_codes_fns
+from modules.egrul.com_f import cnst, get_logger
+from modules.egrul.com_f import all_tables_egrul, all_tables_egrip, get_numb_rows_table
+
+logger = logging.getLogger("DAG_CREATE_ALL_TABLES_IMF_WB")
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.DEBUG)
+
+logger.addHandler(console_handler)
+
+# logger.warning("<========= PARSER START =========>")
+
+# #COMMON VARIABLES
+INPUT_EGRUL = "ЕГРЮЛ_ОТКР_СВЕД"
+INPUT_EGRIP = "ЕГРИП_ОТКР_СВЕД"
+VERS_FORMAT_EGRUL = "4.07"
+VERS_FORMAT_EGRIP = "4.06"
+
+# COMMON VARIABLES -CV
+CV = cnst()
+
+
+class Egrul_egrip:
+
+    def __init__(self) -> None:
+        self.path_file = ()
+
+    def read_egrul_egrip_file_xml(self, name_file ):
+        """Reads messages from a Kafka topic, parses them, and writes data to a database."""
+        logger.warning("START")
+        CODES_FNS = get_codes_fns()
+        print("CODES_FNS = >", CODES_FNS)
+        with open(name_file, "r", encoding="utf-8") as i_f:
+            dataf = i_f.read()
+            print(len(dataf))
+            # Extracting data using a single split operation
+            type_data = dataf.split('ТипИнф="')[1].split('" ВерсПрог=')[0]
+            vers_form = dataf.split('ВерсФорм="')[1].split('" ТипИнф=')[0]
+            kol_doc = int(dataf.split('КолДок="')[1].split('">')[0].strip())
+            print(type_data)
+            print(vers_form)
+            print(kol_doc)
+            if 'ТипИнф="' in dataf and 'ВерсФорм="' in dataf:
+                type_data = dataf.split('ТипИнф="')[1].split('" ВерсПрог=')[0]
+                vers_form = dataf.split('ВерсФорм="')[1].split('" ТипИнф=')[0]
+                print(f"T Y P E   D A T A ================>> {type_data}")
+                print(f"F O R M A T   D A T A ================>> {vers_form}")
+                # token = get_token(token)
+                # CV["gar"]["headers"]["Authorization"] = token["access_token"]
+                if type_data == INPUT_EGRUL and vers_form == VERS_FORMAT_EGRUL:
+                    parser_mesage_egrul(dataf, CODES_FNS, CV)
+                # if type_data == INPUT_EGRIP and vers_form == VERS_FORMAT_EGRIP:
+                #     parser_mesage_egrip(dataf, CODES_FNS, CV)
+            else:
+                logger.warning(
+                    "Message format is incorrect or missing required fields."
+                )
+                print("Error dataf ===>>>", dataf)
+
