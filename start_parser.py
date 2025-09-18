e2f52cca0d71ba79091490b126a5af973790cb4c
gansior <gansior@exportcenter.ru>
2025-09-18 17:59:31 +0300

    add ch

--- start_parser.py
+++ start_parser.py
@@ -12,17 +12,26 @@ from base_egrul_egrip import Egrul_egrip
 
 if __name__ == '__main__':
     b_egr = Egrul_egrip()
-    list_razd = [['Файл', 'Документ', 'СвЮЛ', 'СвАдресЮЛ',	'АдресРФ']]
+    # list_razd = [['Файл', 'Документ', 'СвЮЛ', 'СвАдресЮЛ',	'АдресРФ']]
+    list_razd = []
     for name_file in [
-        os.path.join(new_path, 'dataset', 'egrul', 'push_mes_egrul.xml'),
+        # os.path.join(new_path, 'dataset', 'egrul', 'push_mes_egrul.xml'),
         # os.path.join(new_path, 'dataset', 'egrip', 'push_mes_egrip.xml')
         # os.path.join(new_path, 'dataset', 'egrul', 'error_push_egrul.xml')
         # os.path.join(new_path, 'dataset', 'egrip', '304010131600079.xml')
+        os.path.join(new_path, 'dataset', 'egrip', '323420500054722_error.xml')
     ]:
-        for razd in list_razd:
+        if list_razd:
+            for razd in list_razd:
+                print("name_file ", name_file)
+                print("razd ", razd)
+                if os.path.exists(name_file):
+                    b_egr.read_egrul_egrip_file_xml(name_file, razd)
+                else:
+                    print(f"File not found: {name_file}")
+        else:
             print("name_file ", name_file)
-            print("razd ", razd)
             if os.path.exists(name_file):
-                b_egr.read_egrul_egrip_file_xml(name_file, razd)
+                b_egr.read_egrul_egrip_file_xml(name_file, None)
             else:
                 print(f"File not found: {name_file}")
